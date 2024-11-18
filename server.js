9e46f5260ff72045bbec434b588577a603a45fe5
Marko Dananic <markodananic@MacBook-Pro-od-Marko.local>
2024-11-18 18:53:38 +0100

    added .gitattributes

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+const basePath = path.join(__dirname, 'public'); 
+
+const mimeTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+};
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(basePath, req.url === '/' ? 'index.html' : req.url);
+
+    const ext = path.extname(filePath);
+    const contentType = mimeTypes[ext] || 'application/octet-stream';
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 Not Found</h1>');
+            } else {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end('<h1>500 Internal Server Error</h1>');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data, 'utf-8');
+        }
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
